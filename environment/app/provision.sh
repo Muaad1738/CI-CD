fcaec1602dcd337af325268e5580be784cf71d88
Muaad <Mwarsame@spartaglobal.com>
2024-05-13 11:16:20 +0100

    commit

--- environment/app/provision.sh
+++ environment/app/provision.sh
@@ -12,8 +12,8 @@ sudo apt-get install git -y
 
 # install nodejs
 sudo apt-get install python-software-properties -y
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install nodejs -y
+
+curl -fsSL https://deb.nodesource.com/setup_10.x | sudo DEBIAN_FRONTEND=noninteractive -E bash - && sudo DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs
 
 # install pm2
 sudo npm install pm2 -g
